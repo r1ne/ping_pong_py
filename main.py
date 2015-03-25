6b91fd6eb03483aa8cba6fdf9f2bfea9cbbb51b0
r1ne <mcr1ne90@gmail.com>
2015-03-25 20:33:35 +0600

    Second commit

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import Tkinter
 import tkMessageBox
 import random
 
-class Ball()
+#class Ball():
+
+def draw_rectangle(x1,y1,x2,y2):
+    w.create_rectangle(x1,y1,x2,y2, fill = "Black")
 
 def print_hello(event):
     tkMessageBox.showinfo("Hello","Kak cac?")
 
 def render():
-    #w.create_rectangle(random.randint(10,100),20,200,300, fill = "#000000")
     master.after(50,render)
 
 master = Tkinter.Tk()
@@ -17,6 +19,7 @@ w = Tkinter.Canvas(master, width = 800, height = 600)
 
 w.pack()
 master.bind("<Return>", print_hello)
+draw_rectangle(10,10,100,100)
 
 w.config(background = "#333333", borderwidth = 0, relief = Tkinter.GROOVE)
 render()
