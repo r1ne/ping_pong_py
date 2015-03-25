688ef3897a5ea930b7188df2a0ee558a76f7cfc3
r1ne <mcr1ne90@gmail.com>
2015-03-25 22:41:53 +0600

    I dunno what to write in dis message

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import random
 
 #class Ball():
 
-#def draw_rectangle(x1,y1,x2,y2):
-    #w.create_rectangle(x1,y1,x2,y2, fill = "Black")
-
 def print_hello(event):
     tkMessageBox.showinfo("Hello","Kak cac?")
 
@@ -19,8 +16,8 @@ w = Tkinter.Canvas(master, width = 800, height = 600)
 
 w.pack()
 master.bind("<Return>", print_hello)
-draw_rectangle(10,10,100,100)
+master.bind("<Return>", w.create_rectangle(10,10,50,50, fill = "Black"))
 
-w.config(background = "#333333", borderwidth = 0, relief = Tkinter.GROOVE)
+w.config(background = "#333333", borderwidth = 0)
 render()
 Tkinter.mainloop()
