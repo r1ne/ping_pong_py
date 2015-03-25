7b552ee700584e6d980b8e9ff52e944d12ad252c
r1ne <mcr1ne90@gmail.com>
2015-03-26 00:03:18 +0600

    The beginning of the greatest game evar

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ import Tkinter
 import tkMessageBox
 import random
 
-#class Ball():
+"""TODO:
+1)Запилить класс игровых объектов GameObject(методы - draw(), поля - x,y,width,height,bgcolor)
+2)Запилить классы-потомки GameObject'a (Ball,Racket,Bonus)
+3)Bitmap?
+4)Больше прогить
+"""
+class Ball:
+    def __init__(self):
+
+    def __del__
 
 def print_hello(event):
     tkMessageBox.showinfo("Hello","Kak cac?")
@@ -10,13 +19,16 @@ def print_hello(event):
 def render():
     master.after(50,render)
 
+def draw_rectangle(event):
+    w.create_rectangle(10,10,50,50, fill = "Black")
+
 master = Tkinter.Tk()
 
 w = Tkinter.Canvas(master, width = 800, height = 600)
 
 w.pack()
 master.bind("<Return>", print_hello)
-master.bind("<Return>", w.create_rectangle(10,10,50,50, fill = "Black"))
+master.bind("<Up>", draw_rectangle)
 
 w.config(background = "#333333", borderwidth = 0)
 render()
