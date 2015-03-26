e2828940be54eb4ca2e394a5efbd1a175221d0dd
r1ne <mcr1ne90@gmail.com>
2015-03-26 21:54:35 +0600

    Added hit_detection(), modified move(), we have eaten shit :(

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import Tkinter
 import tkMessageBox
 import random
+import math
 
 """TODO:
 1)Запилить класс игровых объектов GameObject(методы - draw(), поля - x,y,width,height,bgcolor)
@@ -55,24 +56,38 @@ class GameObject:
 class Ball(GameObject):
     def __init__(self, canvas, gameObjectsList):
         #объявляем новые поля
-        self.speed = 0
-        self.angle = 0 #пусть будет в радианах
-        self.deltaX = 0
-        self.deltaY = 0
+        self.speed = random.randint(10,30)
+        self.angle = random.randint(0,360) #пусть будет в градусах
+        projections = self.calc_projections(self.speed, self.angle)
+        self.deltaX = projections[0]
+        self.deltaY = projections[1]
+
 
         #и определяем поля родителя
-        self.x = 100
-        self.y = 100
+        self.x = random.randint(10,740)
+        self.y = random.randint(10,540)
         self.height = 10
-        self.width = 10
+        self.width = self.height
         self.bgcolor = "#ffffff"
         self.canvas = canvas #self.canvas - поле класса, canvas - аргумент при вызове конструктора
         self.gameObjectsList = gameObjectsList
 
         self.move()
-
     def __del__(self):
         pass
+
+    def rad_to_degr(self, angle):
+        return angle * 180 / math.pi
+
+    def degr_to_rad(self, angle):
+        return angle * math.pi / 180
+
+    def calc_projections(self, speed, angle):
+        output = []
+        output.append(speed * math.cos(self.degr_to_rad(angle)))
+        output.append(speed * math.sin(self.degr_to_rad(angle)))
+
+        return output
 
     def draw(self):
         #т.к. в функцию create_rectangle надо передавать не x, y, width, height, а координаты прямоугольника
@@ -86,13 +101,38 @@ class Ball(GameObject):
         pass
 
     def move(self):
-        self.x = self.x + 10
+        p = self.hit_detection()
+        if p != 0:
+            if (self.x + self.width >= p.x) or (self.x <= p.x + p.width):
+                self.deltaX = -self.deltaX
+            elif (self.y + self.height >= p.y) or (self.y <= p.y + p.height):
+                self.deltaY = -self.deltaY
+
+        if self.x <= 0 or self.x >= self.canvas.winfo_reqwidth() - self.width:    #обрабатываем выход за поле
+            self.deltaX = -self.deltaX
+        if self.y <= 0 or self.y >= self.canvas.winfo_reqheight() - self.height:
+            self.deltaY = -self.deltaY
+
+        self.x = self.x + self.deltaX
+        self.y = self.y + self.deltaY
         canvas.after(50, self.move)
 
+    def hit_detection(self):
+        for item in gameObjectsList:
+            if item == self:
+                continue
+            if (self.x + self.width + self.deltaX >= item.x) and \
+              (self.x + self.deltaX <= item.x + item.width) and \
+              (self.y + self.deltaY + self.height >= item.y) and \
+              (self.y + self.deltaY <= item.y + item.height):
+                return item
+
+        return 0
 
 class Racket(GameObject):
     def __init__(self, canvas, gameObjectsList):
-        self.speed
+        self.speed = 50 #скорость движения ракетки в пикселях (быстро/медленно?)
+
 
     def __del__(self):
         pass
@@ -107,7 +147,7 @@ class Racket(GameObject):
 
 class Bonus(GameObject):
     def __init__(self, canvas, gameObjectsList):
-        self.type
+        self.type = 0
 
     def __del__(self):
         pass
@@ -133,14 +173,30 @@ def render():
 
 #Создаем форму, в ней - канвас
 master = Tkinter.Tk()
-canvas = Tkinter.Canvas(master, width = 800, height = 600)
+canvas = Tkinter.Canvas(master, width = 800, height = 600, bd = 0, highlightthickness = 0, relief= 'ridge')
+#canvas.update_idletasks()
 canvas.pack()
-canvas.config(background = "#111111", borderwidth = 0)
+canvas.config(background = "#111111", closeenough = 10)
+master.resizable(width = False, height = False)
 
 #Создаем игровые объекты и добавляем их в список
 gameObjectsList = []
 gameObjectsList.append(Ball(canvas, gameObjectsList))
-
+gameObjectsList.append(Ball(canvas, gameObjectsList))
+gameObjectsList.append(Ball(canvas, gameObjectsList))
+gameObjectsList.append(Ball(canvas, gameObjectsList))
+gameObjectsList.append(Ball(canvas, gameObjectsList))
+gameObjectsList.append(Ball(canvas, gameObjectsList))
+gameObjectsList.append(Ball(canvas, gameObjectsList))
+gameObjectsList.append(Ball(canvas, gameObjectsList))
+bb = Ball(canvas, gameObjectsList)
+bb.width = 30
+bb.height = 30
+bb.x = 300
+bb.y = 350
+bb.bgcolor = "#ff0000"
+gameObjectsList.append(bb)
 #запускаем таймер с отрисовкой
 render()
+#tkMessageBox.showinfo("Title", canvas.winfo_reqheight() - 10)
 Tkinter.mainloop()
